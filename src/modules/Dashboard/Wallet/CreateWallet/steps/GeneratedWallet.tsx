8745d5ddeb77319b0f0766c869518779262e5b57
Leonardo Gardini <leogardini10.1@gmail.com>
2021-08-01 07:27:46 -0300

    DEX-94 - Added a modal option to lock the encrypted seed into application

--- src/modules/Dashboard/Wallet/CreateWallet/steps/GeneratedWallet.tsx
+++ src/modules/Dashboard/Wallet/CreateWallet/steps/GeneratedWallet.tsx
@@ -4,12 +4,14 @@ import {useDispatch} from 'react-redux';
 
 import Box from '@material-ui/core/Box';
 import Fade from '@material-ui/core/Fade';
+import Switch from '@material-ui/core/Switch';
 import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import FileCopy from '@material-ui/icons/FileCopyOutlined';
 import CheckCircle from '@material-ui/icons/CheckCircleOutlineOutlined';
@@ -29,12 +31,14 @@ const GeneratedWallet: React.FC<any> = ({mnemonics, passphrase}) => {
   const dispatch = useDispatch<AppDispatch>();
   const wallet = generateWallet(mnemonics, passphrase);
 
+  const [lockSeed, setLockSeed] = useState(false);
   const [toClipboard, setToClipboard] = useState(false);
 
   useEffect(() => {
     // Stores the encrypted seed into the Redux
-    dispatch(setBTCAccount(wallet));
-  });
+    dispatch(setBTCAccount(lockSeed ? undefined : wallet));
+    // eslint-disable-next-line
+  }, [lockSeed]);
 
   return (
     <Container maxWidth='sm'>
@@ -60,6 +64,18 @@ const GeneratedWallet: React.FC<any> = ({mnemonics, passphrase}) => {
               </Fade>
             </InputAdornment>
           }
+        />
+      </Box>
+      <Box>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={lockSeed}
+              onChange={() => setLockSeed((prev) => !prev)}
+              color='primary'
+            />
+          }
+          label='Lock wallet'
         />
       </Box>
     </Container>
