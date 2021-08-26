77bdce49c8d742d3675bbe0678e1ea7614f2f90e
Aécio Júnior <aecio@dexkit.com>
2021-08-26 19:08:19 -0300

    Fix git bug DEX-19

--- src/modules/Dashboard/Wallet/Accounts/components/AccountListItem.tsx
+++ src/modules/Dashboard/Wallet/Accounts/components/AccountListItem.tsx
@@ -3,12 +3,10 @@ import React, {useCallback, useState, useEffect} from 'react';
 import {UIAccount} from 'redux/_ui/reducers';
 import {truncateAddress} from 'utils';
 import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
+  Grid,
+  Paper,
+  Radio,
   TextField,
-  ListItemSecondaryAction,
-  Tooltip,
   useMediaQuery,
   Theme,
   Typography,
@@ -16,34 +14,64 @@ import {
   IconButton,
   useTheme,
   Chip,
+  Tooltip,
   Box,
+  makeStyles,
+  Checkbox,
 } from '@material-ui/core';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import DoneIcon from '@material-ui/icons/Done';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {ReactComponent as EditIcon} from 'assets/images/icons/edit.svg';
+import clsx from 'clsx';
+import {Home} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+  },
+  paperDefault: {
+    borderWidth: 1,
+    borderColor: '#525C75',
+    borderStyle: 'solid',
+  },
+}));
 
 interface AccountListItemProps {
   account: UIAccount;
-  editing: boolean;
   isConnected: boolean;
   isDefault: boolean;
+  selectActive: boolean;
+  selected: boolean;
+  onSelect: (account: UIAccount) => void;
   onLabelChange: (account: UIAccount, newLabel: string) => void;
   onOpenMenu: (target: HTMLButtonElement, account: UIAccount) => void;
+  onMakeDefault: (account: UIAccount) => void;
 }
 
 export const AccountListItem = (props: AccountListItemProps) => {
   const {
     account,
-    editing,
     isConnected,
     isDefault,
+    selectActive,
+    onSelect,
     onLabelChange,
+    onMakeDefault,
     onOpenMenu,
+    selected,
   } = props;
 
   const [label, setLabel] = useState('');
 
+  const [isEditing, setIsEditing] = useState(false);
+
   const theme = useTheme();
+
+  const classes = useStyles();
 
   const isDesktop = useMediaQuery<Theme>((theme) =>
     theme?.breakpoints.up('sm'),
@@ -60,24 +88,34 @@ export const AccountListItem = (props: AccountListItemProps) => {
     (e: React.MouseEvent) => {
       onLabelChange(account, label);
       setLabel(account.label);
+      setIsEditing(false);
     },
     [account, label],
   );
 
-  const handleOpenMenu = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement>) => {
-      onOpenMenu(e.currentTarget, account);
-    },
-    [account, onOpenMenu],
-  );
+  const handleEdit = useCallback(() => {
+    setIsEditing(true);
+  }, []);
+
+  const handleSelect = useCallback(() => {
+    onSelect(account);
+  }, [onSelect, account]);
+
+  const handleMakeDefault = useCallback(() => {
+    onMakeDefault(account);
+  }, [onMakeDefault, account]);
 
   useEffect(() => {
     setLabel(account.label);
   }, [account]);
 
   return (
-    <ListItem>
-      {editing ? (
+    <Paper
+      className={clsx(
+        classes.paper,
+        isDefault ? classes.paperDefault : undefined,
+      )}>
+      {isEditing ? (
         <TextField
           fullWidth
           label='Account label'
@@ -96,54 +134,63 @@ export const AccountListItem = (props: AccountListItemProps) => {
           onChange={handleLabelChange}
         />
       ) : (
-        <>
-          <ListItemIcon>
-            <Tooltip title={isConnected ? 'Connected' : ''}>
-              <FiberManualRecordIcon
-                style={{
-                  color: isConnected ? green[500] : theme.palette.grey[500],
-                }}
-              />
-            </Tooltip>
-          </ListItemIcon>
-          <ListItemText
-            primary={
-              <Box display='flex' alignItems='center' alignContent='center'>
-                <span style={{paddingRight: theme.spacing(2)}}>
-                  {!isDesktop && account.label == account.address
-                    ? truncateAddress(account.label)
-                    : account.label}
-                </span>
-                {isDefault ? (
-                  <Chip
-                    style={{fontSize: theme.spacing(2)}}
-                    variant='outlined'
-                    size='small'
-                    label='Default'
+        <Grid container alignItems='center' spacing={4}>
+          <Grid item>
+            <Box
+              display='flex'
+              alignItems='center'
+              alignContent='center'
+              justifyContent='center'>
+              {selectActive ? (
+                <Checkbox
+                  checked={selected}
+                  color='primary'
+                  onClick={handleSelect}
+                />
+              ) : (
+                <Tooltip title={isConnected ? 'Connected' : ''}>
+                  <FiberManualRecordIcon
+                    style={
+                      isConnected
+                        ? {color: theme.palette.success.main}
+                        : undefined
+                    }
                   />
-                ) : (
-                  ''
-                )}
-              </Box>
-            }
-            secondary={
-              account.address != account.label ? (
-                <Typography variant='caption' color='textSecondary'>
-                  {isDesktop
-                    ? account.address
-                    : truncateAddress(account.address)}
-                </Typography>
-              ) : null
-            }
-          />
-          <ListItemSecondaryAction>
-            <IconButton onClick={handleOpenMenu}>
-              <MoreVertIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </>
+                </Tooltip>
+              )}
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box display='flex' alignItems='center' alignContent='center'>
+              <span style={{paddingRight: theme.spacing(2)}}>
+                {!isDesktop && account.label == account.address
+                  ? truncateAddress(account.label)
+                  : account.label}
+              </span>
+            </Box>
+            {account.address != account.label ? (
+              <Typography variant='caption' color='textSecondary'>
+                {isDesktop ? account.address : truncateAddress(account.address)}
+              </Typography>
+            ) : null}
+          </Grid>
+          {!selectActive ? (
+            <Grid item>
+              <IconButton onClick={handleEdit} size='small'>
+                <EditIcon />
+              </IconButton>
+            </Grid>
+          ) : null}
+          {!isDefault && !selectActive ? (
+            <Grid item>
+              <IconButton onClick={handleMakeDefault} size='small'>
+                <Home />
+              </IconButton>
+            </Grid>
+          ) : null}
+        </Grid>
       )}
-    </ListItem>
+    </Paper>
   );
 };
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,8 @@
 declare module 'solc';
 declare module 'solc/wrapper';
 
+declare module 'react-swipeable-views';
+
 declare module 'worker-loader!*' {
   // You need to change `Worker`, if you specified a different value for the `workerType` option
   class WebpackWorker extends Worker {
