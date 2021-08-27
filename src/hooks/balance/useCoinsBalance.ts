11ea7ba814642de6ecf4f5d64b75921efdee6ba1
JoaoCampos89 <jcampos893@gmail.com>
2021-08-27 17:14:33 -0300

    missed hook

--- src/hooks/balance/useCoinsBalance.ts
+++ src/hooks/balance/useCoinsBalance.ts
@@ -2,17 +2,20 @@ import { useQuery } from 'react-query';
 import { COINS_SERVICE } from 'services/coins';
 import { useDefaultCoinsAddress } from 'hooks/coins/useDefaultCoinsAddress';
 
+
+
 // Get balance from Coins directly from API
 export const useCoinsBalance = () => {
   const {btcAddress} = useDefaultCoinsAddress();
 
   const {data: btcBalance} = useQuery(['GetBTCBalance', btcAddress], () => btcAddress ?  COINS_SERVICE['bitcoin'].getBalance(btcAddress as string) : undefined );
-
   return {
+    
+
     btc: {
       ...COINS_SERVICE['bitcoin'].getCoinData(),
       balance: btcBalance,
-    }
+    } 
   }
 
   
