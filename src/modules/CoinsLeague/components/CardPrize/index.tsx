c15e3232cc658f545f3bc4237fb576566bb013de
Leonardo Gardini <leogardini10.1@gmail.com>
2021-08-28 15:02:23 -0300

    Created the prize pool card component

---
+++ src/modules/CoinsLeague/components/CardPrize/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    color: '#fff',
+    borderRadius: 6,
+    background: '#2e3243',
+    padding: theme.spacing(2),
+  },
+  innerContent: {
+    padding: theme.spacing(1),
+    justifyContent: 'space-between',
+    fontSize: '1rem',
+  },
+}));
+
+interface Props {
+  prizePool: number;
+}
+
+function CardPrize(props: Props): JSX.Element {
+  const classes = useStyles();
+
+  const value = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(props.prizePool);
+
+  return (
+    <Container className={classes.container} maxWidth='xs'>
+      <Grid container className={classes.innerContent}>
+        <Grid item>
+          <Typography variant='subtitle2' style={{color: '#7A8398'}}>
+            Prize Pool
+          </Typography>
+          <Typography variant='h4'>{value}</Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default CardPrize;
