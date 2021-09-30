eeb0b04bd52bfa57e6a2886d0603514588188198
JoaoCampos89 <jcampos893@gmail.com>
2021-09-30 18:43:43 -0300

    beta testing suggestions

---
+++ src/hooks/useAccountLabel.ts
@@ -0,0 +1,27 @@
+import {useSelector} from 'react-redux';
+import {AppState} from 'redux/store';
+import {Network, SupportedNetworkType} from 'types/blockchain';
+import {isSupportedWalletType} from 'utils/wallet';
+import { useDefaultLabelAccount } from './useDefaultLabelAccount';
+
+export const useAccountLabel = (account?: string, network?: Network) => {
+  let type: SupportedNetworkType = SupportedNetworkType.evm;
+  if (network && isSupportedWalletType(network)) {
+    type = (network as any) as SupportedNetworkType;
+  }
+  const label = useDefaultLabelAccount();
+  const wallet = useSelector<AppState, AppState['ui']['wallet']>(
+    (state) => state.ui.wallet,
+  );
+  if(!account){
+      return label;
+  }
+
+  const accounts = wallet[type];
+  if (accounts && accounts.length) {
+    const acc = accounts.find(a=> a.address.toLowerCase() === account.toLowerCase());
+    if(acc){
+      return acc.label || acc.address;
+    } 
+  }
+};
