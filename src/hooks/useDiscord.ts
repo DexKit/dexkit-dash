711c49e2f99040841607320e183669e9b9c9a059
JoaoCampos89 <jcampos893@gmail.com>
2021-10-13 18:09:53 -0300

    fix discord

--- src/hooks/useDiscord.ts
+++ src/hooks/useDiscord.ts
@@ -1,27 +1,28 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
+
+let called = false;
 
 const useDiscord = () => {
-
   useEffect(() => {
-    const script = document.createElement('script');
+    let script: any;
+    if (!called) {
+      script = document.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
+      script.async = true;
+      const scriptText = document.createTextNode(`
+        new Crate({
+            server: '883248754281574431',
+            channel: '894833805372825620',
+                shard: 'https://emerald.widgetbot.io'
+          });`);
+      script.appendChild(scriptText);
 
-    script.src = "https://cdn.jsdelivr.net/npm/@widgetbot/crate@3";
-    script.async = true;
-    const scriptText = document.createTextNode(`
-     new Crate({
-        server: '883248754281574431',
-        channel: '894833805372825620',
-            shard: 'https://emerald.widgetbot.io'
-      });`);
-
-    script.appendChild(scriptText);
-
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
+      document.body.appendChild(script);
+      called = true;
+      console.log('called');
     }
+
+    return () => {};
   }, []);
 };
 
