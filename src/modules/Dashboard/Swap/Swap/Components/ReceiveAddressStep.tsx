57b9db8b2b7b0f8e93a28086c9d37a214ff25c71
Aécio Júnior <aecio@dexkit.com>
2021-11-24 09:51:12 -0300

    Fix UX problems on multidexswap

--- src/modules/Dashboard/Swap/Swap/Components/ReceiveAddressStep.tsx
+++ src/modules/Dashboard/Swap/Swap/Components/ReceiveAddressStep.tsx
@@ -12,9 +12,23 @@ import {ReceiveAddressInput} from './ReceiveAddressInput';
 
 import SwapVertIcon from '@material-ui/icons/SwapVert';
 import {Alert} from '@material-ui/lab';
+import {ChangellyCoin} from 'types/changelly';
+
+function getChangellyBlockChainName(blockchain?: string, defaultName?: string) {
+  if (blockchain) {
+    if (blockchain === 'ethereum') {
+      return 'Ethereum';
+    } else if (blockchain === 'bitcoin') {
+      return 'Bitcoin';
+    } else if (blockchain === 'binance_smart_chain') {
+      return 'Binance Smart Chain';
+    }
+  }
+  return defaultName;
+}
 
 interface Props {
-  toCoin: any;
+  toCoin?: ChangellyCoin;
   addressToSend: string;
   onChange: (value: string) => void;
   onPaste: (value: string) => void;
@@ -43,6 +57,15 @@ export const ReceiveAddressStep = (props: Props) => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
+        <Alert severity='info'>
+          NOTE: Changelly will send your tokens to{' '}
+          <strong>
+            {getChangellyBlockChainName(toCoin?.blockchain, toCoin?.fullName)}
+          </strong>{' '}
+          network
+        </Alert>
+      </Grid>
+      <Grid item xs={12}>
         <Typography variant='body1'>Receive Address</Typography>
       </Grid>
       <Grid item xs={12}>
@@ -64,9 +87,18 @@ export const ReceiveAddressStep = (props: Props) => {
           </Grid>
           <Grid item xs>
             <Typography color='textSecondary' variant='body2'>
-            Service provided by <Link href={"https://changelly.com/"} target={'_blank'}>Changelly </Link>
-            By proceeding, you acknowledge and understand that the transaction may trigger <Link href={"https://changelly.com/aml-kyc"} target={'_blank'}>Changelly KYC verification</Link>. 
-            This is usually for transactions of higher value than 0.4 BTC or equivalent. Proceeding here will NOT withdraw funds from your account nor will you be obliged to continue the transaction”
+              Service provided by{' '}
+              <Link href={'https://changelly.com/'} target={'_blank'}>
+                Changelly{' '}
+              </Link>
+              By proceeding, you acknowledge and understand that the transaction
+              may trigger{' '}
+              <Link href={'https://changelly.com/aml-kyc'} target={'_blank'}>
+                Changelly KYC verification
+              </Link>
+              . This is usually for transactions of higher value than 0.4 BTC or
+              equivalent. Proceeding here will NOT withdraw funds from your
+              account nor will you be obliged to continue the transaction”
             </Typography>
           </Grid>
         </Grid>
