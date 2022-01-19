7f005ac6570b289f9a5f1d252a9fe487cd740f3f
Aécio Júnior <aecio@dexkit.com>
2022-01-19 10:48:11 -0300

    Resolve problem with opensea linkk

--- src/utils/opensea.ts
+++ src/utils/opensea.ts
@@ -1,4 +1,5 @@
 import {Network, OpenSeaPort} from 'opensea-js';
+import {ChainId} from 'types/blockchain';
 
 // FIXME: use another source
 export const RINKEBY_NETWORK = 0x4;
@@ -32,4 +33,30 @@ export async function getOpenSeaPort(provider: any) {
   const port = new OpenSeaPort(provider, config);
 
   return port;
+}
+
+const KLAYTN_CHAIN_ID = 8217;
+
+export function isOpenSeaSupported(chainId: number) {
+  return (
+    chainId === ChainId.Mainnet ||
+    chainId === ChainId.Matic ||
+    chainId === KLAYTN_CHAIN_ID
+  );
+}
+
+export function getOpenSeaLink(
+  chainId?: number,
+  contractAddress?: string,
+  tokenId?: string,
+) {
+  if (chainId === ChainId.Mainnet) {
+    return `https://opensea.io/assets/${contractAddress}/${tokenId}`;
+  } else if (chainId === ChainId.Matic) {
+    return `https://opensea.io/assets/matic/${contractAddress}/${tokenId}`;
+  } else if (chainId === KLAYTN_CHAIN_ID) {
+    return `https://opensea.io/assets/klaytn/${contractAddress}/${tokenId}`;
+  }
+
+  return '';
 }
