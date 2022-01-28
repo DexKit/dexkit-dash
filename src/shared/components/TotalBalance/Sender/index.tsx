86e7f704c6b224e2235c64980a89859de5ebd34c
Aécio Júnior <aecio@dexkit.com>
2022-01-28 18:40:45 -0300

    Add view transaction and make some adjustments in the code

--- src/shared/components/TotalBalance/Sender/index.tsx
+++ src/shared/components/TotalBalance/Sender/index.tsx
@@ -1,4 +1,7 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
+import clsx from 'clsx';
+
+import IntlMessages from '@crema/utility/IntlMessages';
 
 import {
   Dialog,
@@ -6,6 +9,10 @@ import {
   DialogContent,
   useMediaQuery,
   useTheme,
+  Box,
+  Grid,
+  Typography,
+  Button,
 } from '@material-ui/core';
 import {Fonts} from 'shared/constants/AppEnums';
 import {CremaTheme} from 'types/AppContextPropsType';
@@ -16,6 +23,11 @@ import {ExportWhiteIcon} from 'shared/components/Icons';
 import {Token} from 'types/app';
 import {CustomDialogTitle} from 'shared/components/CustomDialogTitle';
 import {useIntl} from 'react-intl';
+
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+
+import {useChainInfo} from 'hooks/useChainInfo';
+import {useWeb3} from 'hooks/useWeb3';
 
 interface Props {
   open: boolean;
@@ -60,12 +72,25 @@ const useStyles = makeStyles((theme: CremaTheme) => ({
     width: theme.spacing(6),
     height: theme.spacing(6),
   },
+
+  iconLarge: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+  iconSmall: {
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+  },
 }));
 
 const Sender: React.FC<Props> = (props) => {
   const {token: defaultToken, disableClose, onResult, error} = props;
 
+  const [transactionHash, setTransactioHash] = useState<string>();
+
   const classes = useStyles();
+
+  const {chainId} = useWeb3();
   const {onClose} = props;
 
   const handleClose = useCallback(
@@ -73,6 +98,8 @@ const Sender: React.FC<Props> = (props) => {
       if (onClose) {
         onClose();
       }
+
+      setTransactioHash(undefined);
     },
     [onClose],
   );
@@ -82,6 +109,18 @@ const Sender: React.FC<Props> = (props) => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const {messages} = useIntl();
+
+  const handleHash = useCallback((hash: string) => {
+    setTransactioHash(hash);
+  }, []);
+
+  const {getTransactionScannerUrl} = useChainInfo();
+
+  const handleViewTransaction = useCallback(() => {
+    if (chainId && transactionHash) {
+      window.open(getTransactionScannerUrl(chainId, transactionHash), '_blank');
+    }
+  }, [chainId, transactionHash, getTransactionScannerUrl]);
 
   return (
     <Dialog
@@ -98,14 +137,44 @@ const Sender: React.FC<Props> = (props) => {
       />
 
       <DialogContent dividers>
-        <SenderForm
-          balances={props.balances}
-          amount={props.amount}
-          token={defaultToken}
-          address={props.address}
-          onResult={onResult}
-          error={error}
-        />
+        {transactionHash ? (
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Box
+                py={4}
+                display='flex'
+                alignItems='center'
+                alignContent='center'
+                justifyContent='center'>
+                <CheckCircleOutlineIcon
+                  style={{color: theme.palette.success.main}}
+                  className={clsx(classes.iconLarge)}
+                />
+              </Box>
+              <Box mb={2}>
+                <Typography align='center' variant='h5'>
+                  <IntlMessages id='app.wizard.transactionCreated' />
+                </Typography>
+                <Typography align='center' variant='body1'>
+                  <IntlMessages id='app.wizard.pleaseViewTheTransaction' />
+                </Typography>
+              </Box>
+              <Button onClick={handleViewTransaction} color='primary' fullWidth>
+                <IntlMessages id='app.wizard.viewTransaction' />
+              </Button>
+            </Grid>
+          </Grid>
+        ) : (
+          <SenderForm
+            balances={props.balances}
+            amount={props.amount}
+            token={defaultToken}
+            address={props.address}
+            onResult={onResult}
+            onHash={handleHash}
+            error={error}
+          />
+        )}
       </DialogContent>
     </Dialog>
   );
