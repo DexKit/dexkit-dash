4e7a2b7dc00ac9089dc96b153dc5e013fe4d75db
JoaoCampos89 <jcampos893@gmail.com>
2022-02-11 19:42:29 -0300

    init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.coinleague.dexkit.app',
+  appName: 'Coinleague',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
