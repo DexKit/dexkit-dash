e0a8dd6b007799162567d4a3e2d4f1b814546a8f
JoaoCampos89 <j.campos893@gmail.com>
2022-02-14 12:31:41 -0300

    added capacitor configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.coinleague.dexkit.app',
   appName: 'Coinleague',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+ /* server: {
+    url: "http://192.168.0.104:3000",
+    cleartext: true
+  }*/
 };
 
 export default config;
