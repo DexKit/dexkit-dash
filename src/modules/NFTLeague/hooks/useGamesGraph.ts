42cd47022624c47a36a91e499cd1134682618019
JoaoCampos89 <jcampos893@gmail.com>
2022-02-21 09:39:03 -0300

    fix error on types

--- src/modules/NFTLeague/hooks/useGamesGraph.ts
+++ src/modules/NFTLeague/hooks/useGamesGraph.ts
@@ -1,16 +1,17 @@
-import {useWeb3} from 'hooks/useWeb3';
-import {useQuery} from 'react-query';
-import {GameStatus} from '../constants/enum';
-import {GET_ALL_GAMES} from '../services/gql';
-import {getGraphClient} from '../services/graphql';
-import {GameGraph} from '../utils/type';
+import { useWeb3 } from 'hooks/useWeb3';
+import { useQuery } from 'react-query';
+import { GameStatus } from '../constants/enum';
+import { GET_ALL_GAMES } from '../services/gql';
+import { getGraphClient } from '../services/graphql';
+import { GameGraph } from '../utils/types';
+
 
 export const useGamesGraph = (
   status?: GameStatus,
   first: number = 10,
   skip: number = 0,
 ) => {
-  const {chainId} = useWeb3();
+  const { chainId } = useWeb3();
 
   const gamesQuery = useQuery(['GET_GAMES_NFT_LEAGUE', chainId], () => {
     const client = getGraphClient(chainId);
@@ -21,7 +22,7 @@ export const useGamesGraph = (
 
     return client.query<
       GameGraph[],
-      {status: GameStatus; first: number; skip: number}
+      { status: GameStatus; first: number; skip: number }
     >({
       query: GET_ALL_GAMES,
       variables: {
