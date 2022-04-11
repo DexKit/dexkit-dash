0c36b8478b172b0cb9cc8a0a9301eef2617b455e
JoaoCampos89 <jcampos893@gmail.com>
2022-04-11 11:39:18 -0300

    fix login auto route

--- src/shared/components/Web3Manager/index.ts
+++ src/shared/components/Web3Manager/index.ts
@@ -1,16 +1,26 @@
-import {useEffect} from 'react';
-import {useWeb3} from 'hooks/useWeb3';
-import {web3Modal} from 'services/web3modal';
-import {isMobile} from 'web3modal';
-import {useHistory} from 'react-router-dom';
-import {isMagicProvider} from 'services/magic';
-import {useMagicProvider} from 'hooks/provider/useMagicProvider';
+import { useCallback, useEffect } from 'react';
+import { useWeb3 } from 'hooks/useWeb3';
+import { web3Modal } from 'services/web3modal';
+import { isMobile } from 'web3modal';
+import { useHistory, useLocation } from 'react-router-dom';
+import { isMagicProvider } from 'services/magic';
+import { useMagicProvider } from 'hooks/provider/useMagicProvider';
+import { LOGIN_WALLET_ROUTE, WALLET_ROUTE } from 'shared/constants/routes';
 
 export const Web3Manager = () => {
-  const {onConnectWeb3} = useWeb3();
-  const {onConnectMagic} = useMagicProvider();
+  const { onConnectWeb3 } = useWeb3();
+  const { onConnectMagic } = useMagicProvider();
   const history = useHistory();
-  const {web3, ethereum} = window;
+  const { pathname } = useLocation();
+  const { web3, ethereum } = window;
+
+
+  const afterConnectWallet = useCallback((a: string) => {
+    if (pathname === LOGIN_WALLET_ROUTE) {
+      history.push(`${WALLET_ROUTE}/${a}`);
+    }
+
+  }, [pathname, history])
 
   // Try to connect once at begin, check if provider is available and try to connect
   /* eslint-disable */
@@ -25,11 +35,11 @@ export const Web3Manager = () => {
           .then((acc) => {
             // When we connect wallet we want to see our connected wallet
             if (
-              history.location.pathname.startsWith('/wallet') &&
+              history.location.pathname.startsWith(WALLET_ROUTE) &&
               acc &&
               acc.length
             ) {
-              history.push(`/wallet/${acc[0]}`);
+              history.push(`${WALLET_ROUTE}/${acc[0]}`);
             }
           })
           .catch(console.log);
@@ -38,9 +48,10 @@ export const Web3Manager = () => {
       if (
         web3Modal.cachedProvider ||
         (isMobile() && (web3 || ethereum)) ||
-        (ethereum && ethereum?.isMetaMask )
+        (ethereum && ethereum?.isMetaMask)
       ) {
-        onConnectWeb3();
+
+        onConnectWeb3(afterConnectWallet);
       }
     }
   }, []);
